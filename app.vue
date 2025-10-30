82127f68e8416a4fbf321b0cb91cb04ce3d4a80e
ValkyRiE2117 <aritzagk@gmail.com>
2025-10-30 23:28:26 +0700

    add_vercel_analytics

--- app.vue
+++ app.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
+
 <template>
+  <Analytics />
   <div class="bg-gray-50 min-h-screen">
     <Header />
     <NuxtPage />
